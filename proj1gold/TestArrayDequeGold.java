255430e1014e513b5d5c15f522408c4c4a042de4
whattany <wang20210346@163.com>
2025-02-12 00:38:14 +0800

    first try ag

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,63 +7,57 @@ import static org.junit.Assert.*;
 public class TestArrayDequeGold {
     @Test
     public void testTask() {
-        StudentArrayDeque<Integer> stuTest = new StudentArrayDeque<Integer>();
-        ArrayDequeSolution<Integer> testSolution = new ArrayDequeSolution<Integer>();
-        String str = "";
-        int i = 0;
-        while (true) {
+        StudentArrayDeque<Integer> testArray = new StudentArrayDeque<Integer>();
+        ArrayDequeSolution<Integer> solutionArray = new ArrayDequeSolution<Integer>();
+        String log = "";
+        for (int i = 0; i < 1000; i++) {
             int methodNumber = StdRandom.uniform(8);
+            int insertNumber = StdRandom.uniform(100);
             switch (methodNumber) {
                 case 0:
-                    str = str.concat("\nisEmpty()");
-                    assertEquals(str, testSolution.isEmpty(), stuTest.isEmpty());
+                    assertEquals(log, solutionArray.isEmpty(), testArray.isEmpty());
                     break;
                 case 1:
-                    str = str.concat("\nsize()");
-                    assertEquals(str, testSolution.size(), stuTest.size());
+                    assertEquals(log, solutionArray.size(), testArray.size());
                     break;
                 case 2:
-                    int testNum = StdRandom.uniform(stuTest.size() + 10);
-                    str = str.concat("\nget(" + testNum + ")");
-                    if (stuTest.size() == 0 || testNum >= stuTest.size()) {
-                        assertEquals(str, null, stuTest.get(testNum));
+                    int testNum = StdRandom.uniform(testArray.size() + 3);
+                    if (testArray.size() == 0 || testNum >= testArray.size()) {
+                        assertEquals(log, null, testArray.get(testNum));
                     } else {
-                        assertEquals(str, testSolution.get(testNum), stuTest.get(testNum));
+                        assertEquals(log, solutionArray.get(testNum), testArray.get(testNum));
                     }
                     break;
                 case 3:
-                    str = str.concat("\nremoveFirst()");
-                    assertEquals(str, testSolution.removeFirst(), stuTest.removeFirst());
+                    log = log.concat("\nremoveFirst()");
+                    assertEquals(log, solutionArray.removeFirst(), testArray.removeFirst());
                     break;
                 case 4:
-                    str = str.concat("\nremoveLast()");
-                    assertEquals(str, testSolution.removeLast(), stuTest.removeLast());
+                    log = log.concat("\nremoveLast()");
+                    assertEquals(log, solutionArray.removeLast(), testArray.removeLast());
                     break;
                 case 5:
-                    int insertNumber1 = StdRandom.uniform(100);
-                    str = str.concat("\naddFirst(" + insertNumber1 + ")");
-                    stuTest.addFirst(insertNumber1);
-                    testSolution.addFirst(insertNumber1);
+                    log = log.concat("\naddFirst(" + insertNumber + ")");
+                    testArray.addFirst(insertNumber);
+                    solutionArray.addFirst(insertNumber);
                     break;
                 case 6:
-                    int insertNumber = StdRandom.uniform(100);
-                    str = str.concat("\naddLast(" + insertNumber + ")");
-                    stuTest.addLast(insertNumber);
-                    testSolution.addLast(insertNumber);
+                    log = log.concat("\naddLast(" + insertNumber + ")");
+                    testArray.addLast(insertNumber);
+                    solutionArray.addLast(insertNumber);
                     break;
                 case 7:
-                    str = str.concat("\nprintDeque()");
                     ByteArrayOutputStream tempStream = new ByteArrayOutputStream();
                     PrintStream casheStream = new PrintStream(tempStream);
                     PrintStream oldStream = System.out;
                     System.setOut(casheStream);
-                    stuTest.printDeque();
-                    String str1 = tempStream.toString();
+                    testArray.printDeque();
+                    String log1 = tempStream.toString();
                     tempStream.reset();
-                    testSolution.printDeque();
-                    String str2 = tempStream.toString();
+                    solutionArray.printDeque();
+                    String log2 = tempStream.toString();
                     System.setOut(oldStream);
-                    assertEquals(str, str2, str1);
+                    assertEquals(log, log2, log1);
                     break;
                 default:
                     break;
